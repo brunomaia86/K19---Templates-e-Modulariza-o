cac97a7cacab10ab01ba2024dc6d65168935f7ad
Bruno Maia <brunomaia86@gmail.com>
2017-01-13 17:34:31 -0300

    FIM

---
+++ src/controle/InstrutorBean.java
@@ -0,0 +1,36 @@
+package controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import modelo.Instrutor;
+
+@ManagedBean
+@SessionScoped
+public class InstrutorBean {
+
+	private List<Instrutor> instrutores = new ArrayList<Instrutor>();
+
+	public InstrutorBean() {
+		Instrutor rafael = new Instrutor();
+		rafael.setNome("Rafael Cosentino");
+		rafael.setDataDeNascimento("30/10/1984");
+		Instrutor marcelo = new Instrutor();
+		marcelo.setNome("Marcelo Martins");
+		marcelo.setDataDeNascimento("02/04/1985");
+		this.instrutores.add(rafael);
+		this.instrutores.add(marcelo);
+	}
+
+	public List<Instrutor> getInstrutores() {
+		return instrutores;
+	}
+
+	public void setInstrutores(List<Instrutor> instrutores) {
+		this.instrutores = instrutores;
+	}
+
+}
